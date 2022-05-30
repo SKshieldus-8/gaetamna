87117ef5ebeaa7b11cae0ff0fa46de4003b87efe
Eunji Kim <eji33@naver.com>
2022-05-30 23:09:30 +0900

    initial commit

---
+++ GTN/app/src/main/java/com/example/gtn/SecondActivity.kt
@@ -0,0 +1,179 @@
+package com.example.gtn
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
+import android.widget.ImageView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+
+@Suppress("deprecation")
+class SecondActivity : AppCompatActivity(), ActionBar.TabListener {
+    lateinit var tab1: ActionBar.Tab
+    lateinit var tab2: ActionBar.Tab
+    lateinit var tab3: ActionBar.Tab
+    lateinit var tab4: ActionBar.Tab
+
+    var myFrags = arrayOfNulls<MyTabFragment>(4)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.temppage)
+
+        var bar = this.supportActionBar
+        bar!!.navigationMode = ActionBar.NAVIGATION_MODE_TABS
+
+        tab1 = bar.newTab()
+        tab1.text = "전체"
+        tab1.setTabListener(this)
+        bar.addTab(tab1)
+
+        tab2 = bar.newTab()
+        tab2.text = "신분증"
+        tab2.setTabListener(this)
+        bar.addTab(tab2)
+
+        tab3 = bar.newTab()
+        tab3.text = "통장사본"
+        tab3.setTabListener(this)
+        bar.addTab(tab3)
+
+        tab4 = bar.newTab()
+        tab4.text = "증명서"
+        tab4.setTabListener(this)
+        bar.addTab(tab4)
+    }
+
+    override fun onTabSelected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {
+        var myTabFrag: MyTabFragment? = null
+
+        if (tab != null) {
+            if(myFrags[tab.position] == null){
+                myTabFrag = MyTabFragment()
+                val data = Bundle()
+                data.putString("tabName", tab.text.toString())
+                myTabFrag.arguments = data
+                myFrags[tab.position] = myTabFrag
+            }
+            else {
+                myTabFrag = myFrags[tab.position]
+            }
+        }
+
+        ft?.replace(android.R.id.content, myTabFrag!!)
+    }
+
+    override fun onTabUnselected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {}
+    override fun onTabReselected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {}
+
+    class MyTabFragment : androidx.fragment.app.Fragment(){
+        var tabName: String? = null
+
+        override fun onCreate(savedInstanceState: Bundle?){
+            super.onCreate(savedInstanceState)
+            var data = arguments
+            tabName = data!!.getString("tabName")
+        }
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
+            var baseLayout : View? = null
+
+            if(tabName === "전체") baseLayout = inflater.inflate(R.layout.mainpage_tab1, null)
+            if(tabName === "신분증") baseLayout = inflater.inflate(R.layout.mainpage_tab2, null)
+            if(tabName === "통장사본") baseLayout = inflater.inflate(R.layout.mainpage_tab3, null)
+            if(tabName === "증명서") baseLayout = inflater.inflate(R.layout.mainpage_tab4, null)
+            return baseLayout
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            var cont = context
+            // 각 탭 별로 화면 내에서 처리할 이벤트 정의
+            // 해당하는 유형의 사진 출력
+            // 사진이 눌렸을 경우 실행할 이벤트를 함수로 만들어 재사용 할 것
+            //
+
+            if(tabName === "전체") {
+                var gv = view.findViewById<View>(R.id.tab1_gridView) as GridView
+                var gAdapter = MyGridAdapter(context = cont)
+                gv.adapter = gAdapter
+            }
+
+            if(tabName === "신분증") {
+                // 해당되는 이미지만 불러와야함
+                /*
+                var gv = view.findViewById<View>(R.id.tab2_gridView) as GridView
+                var gAdapter = MyGridAdapter(view)
+                gv.adapter = gAdapter
+
+                 */
+            }
+
+            if(tabName === "통장사본") {
+                // 해당되는 이미지만 불러와야함
+                /*
+                var gv = view.findViewById<View>(R.id.tab3_gridView) as GridView
+                var gAdapter = MyGridAdapter(view)
+                gv.adapter = gAdapter
+
+                 */
+            }
+
+            if(tabName === "증명서") {
+                // 해당되는 이미지만 불러와야함
+                /*
+                var gv = view.findViewById<View>(R.id.tab4_gridView) as GridView
+                var gAdapter = MyGridAdapter(view)
+                gv.adapter = gAdapter
+                */
+            }
+        }
+    }
+
+    class MyGridAdapter(var context: Context?) : BaseAdapter() {
+
+        var imageId = arrayOf(
+            R.drawable.img01, R.drawable.img02, R.drawable.img03,
+            R.drawable.img04, R.drawable.img05, R.drawable.img06,
+            R.drawable.img07, R.drawable.img08, R.drawable.img09,
+            R.drawable.img10, R.drawable.img11, R.drawable.img12,
+            R.drawable.img13, R.drawable.img14, R.drawable.img15,
+            R.drawable.img01, R.drawable.img02, R.drawable.img03,
+            R.drawable.img04, R.drawable.img05, R.drawable.img06,
+            R.drawable.img07, R.drawable.img08, R.drawable.img09,
+            R.drawable.img10, R.drawable.img11, R.drawable.img12,
+            R.drawable.img13, R.drawable.img14, R.drawable.img15
+        )
+
+        override fun getCount(): Int {
+            return imageId.size
+        }
+
+        override fun getItem(p0: Int): Any {
+            return 0
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return 0
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            var imageview = ImageView(context)
+            imageview.layoutParams = ViewGroup.LayoutParams(350, 400)
+            imageview.scaleType = ImageView.ScaleType.FIT_CENTER
+            imageview.setPadding(5, 5, 5, 5)
+
+            imageview.setImageResource(imageId[p0])
+
+            // when image clicked, inflate viewpage with intent
+
+
+            return imageview
+        }
+    }
+}
