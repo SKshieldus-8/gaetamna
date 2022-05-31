52fb8881397882ae7c35fd36c012e6c292cc8e09
Lydia <59132060+Lydia-k@users.noreply.github.com>
2022-05-31 10:48:31 +0900

    Add files via upload

---
+++ GTN/app/src/main/java/com/example/gtn/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.gtn
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import java.util.regex.Pattern
+
+@Suppress("deprecation")
+class MainActivity : AppCompatActivity(){
+
+    lateinit var edittext_password: EditText
+    lateinit var btnLogin: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loginpage)
+
+        edittext_password = findViewById<EditText>(R.id.LoginPage_editTextTextPassword)
+        btnLogin = findViewById<Button>(R.id.LoginPage_button)
+
+        btnLogin.setOnClickListener {
+            var password = edittext_password.text.toString()
+            if(password == "password"){
+                // goto mainpage
+                var intent = Intent(applicationContext, SecondActivity::class.java)
+                startActivity(intent)
+            }
+            // 유효성검사
+            else if (!Pattern.matches("^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-zA-Z]).{8,20}$", password)) {
+                Toast.makeText(this, "비밀번호 형식을 지켜주세요.", Toast.LENGTH_SHORT).show()
+                edittext_password.setText("")
+            }
+
+            else {
+                //Toast message
+                Toast.makeText(this, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
+                edittext_password.setText("")
+            }
+        }
+    }
+}
