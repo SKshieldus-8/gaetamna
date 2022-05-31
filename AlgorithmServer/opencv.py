3fbfaab1d5400712a664b568dd6c906fe4cde8d1
kaias <kaiascezar@gmail.com>
2022-05-31 11:59:11 +0900

    preprocessing

--- AlgorithmServer/opencv.py
+++ AlgorithmServer/opencv.py
@@ -1,39 +1,66 @@
+import sys
 import cv2
 import numpy as np
 
 
+def reorderPts(pts):  # 꼭지점 순서 정렬
+    idx = np.lexsort((pts[:, 1], pts[:, 0]))  # 칼럼0 -> 칼럼1 순으로 정렬한 인덱스를 반환
+    pts = pts[idx]  # x좌표로 정렬
+
+    if pts[0, 1] > pts[1, 1]:
+        pts[[0, 1]] = pts[[1, 0]]  # 스와핑
+
+    if pts[2, 1] < pts[3, 1]:
+        pts[[2, 3]] = pts[[3, 2]]  # 스와핑
+
+    return pts
+
+
+# 영상 불러오기
+filename = "C:/gaetamna/AlgorithmServer/registcard_test.png"
+# filename = "registcard_test.png"
+if len(sys.argv) > 1:       #py파일을 실행 시키면서 인자값을 넣으면 그 인자값을 filename으로 지정
+    filename = sys.argv[1]  #ex) opencv2.py ./test.jpg
+
+
 win_name = 'scan'
-img = cv2.imread('./test.jpg')
-draw = img.copy()
+src = cv2.imread(filename)
 
-#Gary 스케일 변환 및 캐니 엣지 검출
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (3, 3), 0)
-edged = cv2.Canny(gray, 300, 300)
-cv2.imshow(win_name, edged)
+# 출력 영상 설정
+dw, dh = 720, 400
+srcQuad = np.array([[0, 0], [0, 0], [0, 0], [0, 0]], np.float32)
+dstQuad = np.array([[0, 0], [0, dh], [dw, dh], [dw, 0]], np.float32)
+dst = np.zeros((dh, dw), np.uint8)
+
+# 입력 영상 전처리
+src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+_, src_bin = cv2.threshold(src_gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)   #영상의 이진화
+
+# 외곽선 검출 및 명함 검출
+contours, _ = cv2.findContours(src_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+cpy = src.copy()
+for pts in contours:
+    # 너무 작은 객체는 무시
+    if cv2.contourArea(pts) < 1000:
+        continue
+
+    # 외곽선 근사화
+    approx = cv2.approxPolyDP(pts, cv2.arcLength(pts, True) * 0.02, True)
+
+    # 컨벡스가 아니고, 사각형이 아니면 무시
+    if not cv2.isContourConvex(approx) or len(approx) != 4:
+        continue
+
+    cv2.polylines(cpy, [approx], True, (0, 255, 0), 2, cv2.LINE_AA)
+    srcQuad = reorderPts(approx.reshape(4, 2).astype(np.float32))
+
+pers = cv2.getPerspectiveTransform(srcQuad, dstQuad)
+dst = cv2.warpPerspective(src, pers, (dw, dh))
+cv2.imshow(win_name, dst)
 cv2.waitKey(0)
 
-#컨투어 찾기
-cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-print(len(cnts))
-cv2.drawContours(draw, cnts, -1, (0, 255, 0))
-cv2.imshow(win_name, draw)
+dst_gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+cv2.imshow(win_name, dst_gray)
 cv2.waitKey(0)
-
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
-for c in cnts:
-    peri = cv2.arcLength(c, True)   #영역이 가장 큰 컨투어부터 근사 컨투어 단순화
-    verticles = cv2.approxPolyDP(c, 0.02 * peri, True)  # 둘레 길이의 0.02 근사값으로 근사화
-    if len(verticles) == 4: # 근사한 꼭짓점이 4개면 중지
-        break
-
-pts = verticles.reshape(20, 1)   # N * 1 * 2 배열을 4 * 2 크기로 조정
-for x, y in pts:
-    cv2.circle(draw, (x,y), 10, (0, 255, 0), -1)
-
-cv2.imshow(win_name, draw)
-cv2.waitKey(0)
-marged = np.hstack((img, draw))
-
-cv2.destroyAllWindows()
